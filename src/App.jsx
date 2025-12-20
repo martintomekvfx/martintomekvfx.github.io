fb97b6ca6140fb73f8e4aafe73d4e6d22abdcace
Themolx <themolxyt@gmail.com>
2025-12-20 17:21:22 +0100

    Remove base path - site now served from root URL

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter basename="/martintomekvfx">
+    <BrowserRouter>
       <ScrollToHash />
       <Routes>
         <Route path="/" element={<Layout />}>
