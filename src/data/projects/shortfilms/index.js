729c8d349a2fce57e001c11f7cfc42ec2b0c8aa4
Themolx <themolxyt@gmail.com>
2026-01-31 02:01:30 +0100

    Add Sonification of Rotating Light project page

--- src/data/projects/shortfilms/index.js
+++ src/data/projects/shortfilms/index.js
@@ -4,11 +4,13 @@
 import hybaj from '../analog/hybaj.js';
 import spider from '../analog/spider.js';
 import footprint from '../analog/footprint.js';
+import sonification from '../analog/sonification.js';
 import originalCopy from '../analog/original-copy.js';
 
 export const shortfilmsProjects = [
     hybaj,
     footprint,
     spider,
+    sonification,
     originalCopy,
 ];
